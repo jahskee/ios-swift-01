ab9c6b10a1c2e315e8c28cca6377b6c0caa10d52
Jaizon Fernando Lubaton <jahskee@yahoo.com>
2019-02-07 18:04:09 -0500

    update

--- IOS-Swift-01/ViewController.swift
+++ IOS-Swift-01/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = "Banana"
     }
 
 
+    @IBAction func pressMe(_ sender: Any) {
+        label.text = "Woooo!"
+    }
 }
 
